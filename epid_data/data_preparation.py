3ba419cbca027e607200aef12e3f3de03c455ad4
Андрей Корзин <andrey@Andrejs-MacBook-Air.local>
2024-09-09 13:52:26 +0300

    First commit

---
+++ epid_data/data_preparation.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+class EpidData:
+    def __init__(self) -> None:
+        self.epid_df = None
+        self.waves = None # list of lists of epidemic waves
+
+    def create_dataframe():
+        epid_df = pd.DataFrame(columns=['year', 'week_number', 'strain_A(H1N1)pdm09_age_0-14_cases', 
+                                        'strain_A(H3N2)_age_0-14_cases',
+                                        'strain_B_age_0-14_cases',
+                                        'population_age_0-14',
+                                        'strain_A(H1N1)pdm09_age_15+_cases', 
+                                        'strain_A(H3N2)_age_15+_cases',
+                                        'strain_B_age_15+_cases',
+                                        'population_age_15+'])
+        # datelist = pd.date_range(start='2010-01', end='2023-12', freq='W').tolist()
+        # epid_df['date'] = datelist
+        # epid_df.to_excel('epid_data_spb.xlsx')
+
+    def read_epid_data(self, filename='epid_data_spb.xlsx'):
+        self.epid_df = pd.read_excel(filename)
+        self.epid_df.fillna(value=0)
+
+    def get_waves(self):
+        incidence = self.epid_df['total'][:-100].to_list()
+        self.waves = []
+        for i in range(10):
+            self.waves.append(incidence[52-25 + 52*i:52+25 + 52*i])
+    
+    def plot_whole_epid_data(self):
+        fig, ax = plt.subplots(figsize=(10,5))
+        ax.plot(self.epid_df['total'][40:-100], '--o')
+        ax.grid()
+        fig.savefig('incidence.png', bbox_inches='tight')
+    
+    def plot_waves(self):
+        fig, ax = plt.subplots(figsize=(10,5))
+        for wave in self.waves:
+            ax.plot(wave, '--o', color='royalblue', alpha=0.6)
+            break
+        ax.grid()
+        fig.savefig('waves.png', bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    epid_data = EpidData()
+    epid_data.read_epid_data()
+    # epid_data.plot_epid_data()
+    epid_data.get_waves()
+    epid_data.plot_waves()
---
+++ models/BR_model.py
@@ -0,0 +1,92 @@
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+
+type Vector = list[float]
+
+class SimpleBRModel:
+    def __init__(self, alpha: float, beta: float, initial_infectious: int,
+                 rho: int, br_func_array: Vector):
+        self.model_name = "SimpleBRModel"
+        self.alpha = alpha # fraction of non-immune
+        self.beta = beta # effective contacts intensivity
+        self.initial_infectious = initial_infectious
+        self.rho = rho # population size 
+        self.br_func_array = br_func_array # Baroyan-Rvachev function of infectiousness
+
+        ### PARAMETERS BELOW ARE SET AFTER SIMULATION
+        self.time = None
+        self.newly_infected = None
+        self.susceptible = None
+        self.total_infected = None
+
+
+    def br_function(self, day: int):
+        if day >= len(self.br_func_array):
+            return 0
+        return self.br_func_array[day]
+        
+
+    def simulate(self, modeling_duration=150):
+        # SETTING UP INITIAL CONDITIONS
+        self.time = np.linspace(1, modeling_duration, modeling_duration)
+        initial_susceptible = int(self.alpha*self.rho)
+        initial_infectious = self.initial_infectious
+        self.total_infected = np.zeros(modeling_duration)
+        self.newly_infected = np.zeros(modeling_duration)
+        self.susceptible = np.zeros(modeling_duration)
+        self.total_infected[0] = initial_infectious 
+        self.newly_infected[0] = initial_infectious
+        self.susceptible[0] = initial_susceptible
+
+        # SIMULATION
+        for day in range(modeling_duration-1):
+            self.total_infected[day] = min(sum([self.newly_infected[day - tau]*self.br_function(tau) 
+                                    for tau in range(len(self.br_func_array)) if (day - tau) >=0]),
+                                    self.rho)
+            
+            self.newly_infected[day+1] = min(self.beta*self.susceptible[day]*self.total_infected[day]/self.rho,
+                                             self.susceptible[day])
+            self.susceptible[day+1] = self.susceptible[day] - self.newly_infected[day+1]            
+        
+    def plot_output(self):
+        fig, ax = plt.subplots(figsize=(10,5))
+        ax.plot(self.newly_infected, '--o')
+        # ax.plot(self.total_infected, '--o')
+        # ax.plot(self.susceptible, '--o')
+        # ax.hlines(self.rho, xmin=self.time[0], xmax=self.time[-1])
+        ax.set_xlim([0, self.time[-1]])
+        ax.legend(['Incidence', 'Prevalence', 'Susceptible'])
+        ax.grid()
+        plt.show()
+        # fig.savefig(str(self.make_params_dict()) + '.png')
+
+    def make_params_dict(self):
+        params_dict = {}
+        params_dict['alpa'] = self.alpha
+        params_dict['beta'] = self.beta
+        params_dict['initial_infectious'] = self.initial_infectious
+        params_dict['rho'] = self.rho
+        params_dict['br_func'] = self.br_func_array
+        return params_dict
+
+    def save_result(self):
+        # TODO: put string of parameter dictionary in the beginning of the file
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        file_name = r'{}_{}.csv'.format(self.model_name, current_time)
+        df = pd.DataFrame(columns=['incidence', 'prevalence', 'susceptible'])
+        df['incidence'] = self.newly_infected
+        df['prevalence'] = self.total_infected
+        df['susceptible'] = self.susceptible
+        df.to_csv(file_name, index=False)
+
+
+if __name__ == '__main__':
+    br_model = SimpleBRModel(alpha=1, beta=0.52, initial_infectious=100, rho=5e6, 
+                             br_func_array=[0.1, 0.1, 1, 0.9, 0.55, 0.3, 0.15, 0.05])
+    br_model.simulate()
+    br_model.plot_output()
+    br_model.save_result()
+    
