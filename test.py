94725cdf8ea86ff8af0ca9dd363473736a7c1195
Андрей Корзин <andrey@Andrejs-MacBook-Air.local>
2024-12-26 16:57:17 +0300

    edit gitignore

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-import pymc as pm
+from model_complex.compartmental_models import Calibration, EpidData, FactoryModel
+import matplotlib.pyplot as plt
 
-print(pm.__version__)
+use_regime = 'total'
+data = EpidData(city='samara', path='./', 
+                start_time='01-07-2015', end_time='20-06-2016', 
+                regime='total')
+init_infected = [100]
+model =  FactoryModel.get_model('BR-total')
+
+d = Calibration(init_infected, model, data)
+idata, data, simulation_func = d.abc_calibration()
+
